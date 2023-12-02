c08deff1e20d22a582a6710e33b7d1ae8d96bc6f
✧˖°.Xorginho✧˖° <144127740+xorginhoo@users.noreply.github.com>
2023-12-02 11:30:25 -0300

    Add files via upload

--- struct4.c
+++ struct4.c
@@ -1,43 +1,43 @@
 #include <stdio.h>
-struct dados_funcionario
+struct dados_aluno
 {
     char nome[200];
-    char admissao[200];
+    char ndaturma[200];
     char matricula[200];
-    char endereco[300];
+    char notas[300];
 };
 
-struct dados_cliente
+struct dados_professor
 {
     char nome[200];
-    char nascimento[200];
-    char endereco[300];
+    char Disciplina[200];
+    char turmas[300];
 };
 
 int main()
 {
     int i;
-    struct dados_funcionario funcionario[3];
-    struct dados_cliente cliente[3];
+    struct dados_aluno funcionario[3];
+    struct dados_professor cliente[3];
     for (i = 0; i < 3; i++)
     {
         printf("       funcionario        \n");
         printf("Digite seu nome: \n");
         gets(funcionario[i].nome);
         printf("Digite sua data de adimssao: \n");
-        gets(funcionario[i].admissao);
+        gets(funcionario[i].ndaturma);
         printf("Digite seu matricula: \n");
         gets(funcionario[i].matricula);
         printf("Digite seu endereço:\n");
-        gets(funcionario[i].endereco);
+        gets(funcionario[i].notas);
 
         printf("cliente\n");
         printf("Digite seu nome:\n");
         gets(cliente[i].nome);
-        printf("Digite sua data de nascimento:\n");
-        gets(cliente[i].nascimento);
+        printf("Digite sua data de Disciplina:\n");
+        gets(cliente[i].Disciplina);
         printf("Digite seu endereço:\n");
-        gets(cliente[i].endereco);
+        gets(cliente[i].turmas);
 
         fflush(stdin);
         system("cls||clear");
@@ -46,13 +46,13 @@ int main()
     for (i = 0; i < 3; i++)
     {
         printf("Nome funcionario: %s \n", funcionario[i].nome);
-        printf("Admissao: %s \n", funcionario[i].admissao);
+        printf("Admissao: %s \n", funcionario[i].ndaturma);
         printf("matricula: %s \n", funcionario[i].matricula);
-        printf("endereço: %s \n", funcionario[i].endereco);
+        printf("endereço: %s \n", funcionario[i].notas);
 
         printf("Nome cliente: %s \n", cliente[i].nome);
-        printf("Data de nascimento: %s \n", cliente[i].nascimento);
-        printf("Endereço: %s \n", cliente[i].endereco);
+        printf("Data de Disciplina: %s \n", cliente[i].Disciplina);
+        printf("Endereço: %s \n", cliente[i].turmas);
     }
 
     return 0;
